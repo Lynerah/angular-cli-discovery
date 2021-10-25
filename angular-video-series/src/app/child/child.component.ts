b60f16301f5478e55eb4fcab4fcaf75a6283ce16
Lynerah <celinedeboeck@gmail>
2021-10-25 09:08:21 +0200

    component styles

--- angular-video-series/src/app/child/child.component.ts
+++ angular-video-series/src/app/child/child.component.ts
@@ -3,7 +3,10 @@ import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 @Component({
   selector: 'app-child',
   templateUrl: './child.component.html',
+  //Way one to style component: setting style ou styleUrl metadata
+  // styles:['h2{color: green}']
   styleUrls: ['./child.component.css']
+
 })
 export class ChildComponent implements OnInit {
 @Input() childMessage!: string;
@@ -17,5 +20,4 @@ constructor() { }
   sendMessage(){
     this.messageEvent.emit('Hello from child');
   }
-
 }
--- angular-video-series/src/app/parent/parent.component.ts
+++ angular-video-series/src/app/parent/parent.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, ViewChild, AfterViewInit } from '@angular/core';
-
+import {ChildComponent} from '../child/child.component';
 @Component({
   selector: 'app-parent',
   templateUrl: './parent.component.html',
@@ -7,10 +7,18 @@ import { Component, OnInit, ViewChild, AfterViewInit } from '@angular/core';
 })
 export class ParentComponent implements OnInit {
 
+  //Communication via ViewChild : @decorateur (nom du composant enfant) nom donnée à l'sensemble
+  // @ViewChild(ChildComponent) child;
+
   constructor() { }
 
   ngOnInit(): void {
   }
+
+  // avec le ViewChild initialiser le hook AfterViewInit
+  // ngAfterViewInit(){
+  //   alert(this.child.message);
+  // }
 
   receiveMessage(msg){
     alert(msg);
